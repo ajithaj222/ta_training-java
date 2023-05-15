aadecfd9451de2045fc11645e9f3892c607f406d
ajithaj222 <93924978+ajithaj222@users.noreply.github.com>
2023-05-15 18:55:55 +0530

    Create MeetAStranger.java

---
+++ com.epam.training.ajith_vangara/MeetAStranger.java
@@ -0,0 +1,10 @@
+package com.epam.training.ajith_vangara;
+import java.util.*;
+public class MeetAStranger {
+    public static void main(String[] args) {
+        //Write a program, which read a String from System.in and print "Hello, <input string>"
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.println("Hello, "+a);
+    }
+}
