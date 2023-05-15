2083cf9fbc81c0088653cd70c4cb52865fb7c21d
ajithaj222 <93924978+ajithaj222@users.noreply.github.com>
2023-05-15 18:54:10 +0530

    Delete HelloAutocode.java

--- HelloAutocode.java
+++
@@ -1,9 +0,0 @@
-package com.epam.training.ajith_vangara;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        //Write a program, printing "Hello, Autocode!"
-        System.out.println("Hello, Autocode!");
-       
-    }
-}
