5546f2bfc46a54cfbd47418b04187ac3c3b042d4
ajithaj222 <93924978+ajithaj222@users.noreply.github.com>
2023-05-15 18:53:10 +0530

    Add files via upload

---
+++ com.epam.training.ajith_vangara/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.epam.training.ajith_vangara;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+        System.out.println("Hello, Autocode!");
+       
+    }
+}
